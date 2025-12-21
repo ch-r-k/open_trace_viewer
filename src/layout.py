88ef42ea1725f37f5199157699fae1fee05ce91b
ch-r-k <christoph_kurz@live.at>
2025-12-21 18:17:43 +0000

    remove layout

--- src/layout.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-class Layout:
-    def __init__(self):
-        st.set_page_config(page_title="Open Trace Viewer", page_icon="📊", layout="wide")
-        st.title("📈 Open Trace Viewer")
